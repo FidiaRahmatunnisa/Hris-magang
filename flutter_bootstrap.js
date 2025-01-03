cacfe188d1ef7b63e0ed7f89140cf5c9826f171e
Fidia Rahmatunnisa <fidiarahmatunnisa274@gmail.com>
2025-01-03 16:27:45 +0700

    memperbaiki subHeader dimana rigth sebelumnya tidak tampil, ternyata perlu widget stack seperti header

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2376798514"
+    serviceWorkerVersion: "3571933676"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -71,14 +71,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "6047d9a27fc14cd412b77e961a5687ac
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "54ad26d6e84041aa132a0159a3bde63a",
+"flutter_bootstrap.js": "295d07a8627a33fa57b9770167875ea6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b70391d1e0851ef45d8f136ee85d7ac5",
-"/": "b70391d1e0851ef45d8f136ee85d7ac5",
-"main.dart.js": "2d3b165c2b8266504da275a377eb7c6a",
+"index.html": "18ce063ae7f70ed561a7c5da8c1d148f",
+"/": "18ce063ae7f70ed561a7c5da8c1d148f",
+"main.dart.js": "5236a99206d192e0cbfa1fff0f74997b",
 "manifest.json": "cc46981f73a1117282b46aabf50fba8d",
 "version.json": "e245cccff8c9c20b0e2b67836e0cbf4a"};
 // The application shell files that are downloaded before a service worker can
