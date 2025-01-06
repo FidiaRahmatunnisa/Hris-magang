209f6bcce1b08171ee5f23da20957cc476fb5b76
Fidia Rahmatunnisa <fidiarahmatunnisa274@gmail.com>
2025-01-06 15:31:12 +0700

    barusan memperbaiki ukuran projek fundamental agar bisa tampil di vercel hosting

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -71,14 +71,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "6047d9a27fc14cd412b77e961a5687ac
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "d4c3cf12601f704eb076e20f5fe6296e",
+"flutter_bootstrap.js": "467e4b8c51813e16cc61dea9175dcd81",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8f6207d59fc57dd54285c362f0c5cd4d",
-"/": "8f6207d59fc57dd54285c362f0c5cd4d",
-"main.dart.js": "9be4791f2b815be0c04cec60703b1560",
+"index.html": "3943d994633c4cacf74d7e375e19a678",
+"/": "3943d994633c4cacf74d7e375e19a678",
+"main.dart.js": "1608e0822568d519ee529ead69068d0e",
 "manifest.json": "cc46981f73a1117282b46aabf50fba8d",
 "version.json": "e245cccff8c9c20b0e2b67836e0cbf4a"};
 // The application shell files that are downloaded before a service worker can
